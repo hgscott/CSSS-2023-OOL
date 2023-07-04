19d72ba0fbfab5e8ffd028b07f118f5dc69e90c1
Helen G Scott <helengracescott@gmail.com>
2023-07-04 14:57:17 -0600

    Remove 0s from the distribution plot

--- make_distribution_plot.py
+++ make_distribution_plot.py
@@ -6,7 +6,7 @@ G = nx.read_graphml('core_network_weighted.graphml')
 
 # Plot the distribution of weights
 # Should I do all edges or just the one per reaction?
-plt.hist([G.edges[edge]['weight'] for edge in G.edges()])
+plt.hist([G.edges[edge]['weight'] for edge in G.edges() if G.edges[edge]['weight'] != 0])
 
 # Save the plot
 plt.savefig('weight_distribution.png')
